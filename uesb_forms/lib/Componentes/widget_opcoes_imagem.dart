c3e242122216ac67421a9db7a6d62c7dc4ef580b
AlexR0drigo <alexrodrigopbdev@gmail.com>
2025-04-22 16:11:16 -0300

    widget corrigido

--- uesb_forms/lib/Componentes/widget_opcoes_imagem.dart
+++ uesb_forms/lib/Componentes/widget_opcoes_imagem.dart
@@ -3,31 +3,27 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
 
+class WidgetOpcoesImagem extends StatefulWidget {
+  final Function(Uint8List?) onImageSelected;
+
+  const WidgetOpcoesImagem({Key? key, required this.onImageSelected})
+      : super(key: key);
+
+  @override
+  _WidgetOpcoesImagemState createState() => _WidgetOpcoesImagemState();
+}
+
 class _WidgetOpcoesImagemState extends State<WidgetOpcoesImagem> {
   final ImagePicker _imagePicker = ImagePicker();
-  Uint8List? _imageData;
 
   Future<void> _pickImage(ImageSource source) async {
-    final pickedFile = await _imagePicker.pickImage(source: source);
     try {
       final pickedFile = await _imagePicker.pickImage(source: source);
 
-    if (pickedFile != null) {
-      // Carregar a imagem
-      final imageBytes = await pickedFile.readAsBytes();
-      // Decodificar a imagem
-      img.Image? originalImage = img.decodeImage(imageBytes);
       if (pickedFile != null) {
         // Carregar a imagem como bytes
         final imageBytes = await pickedFile.readAsBytes();
 
-      // Redimensionar a imagem para uma largura máxima de 100, mantendo a proporção
-      if (originalImage != null) {
-        img.Image resizedImage = img.copyResize(originalImage, width: 100);
-        // Codificar de volta em bytes
-        final resizedBytes = img.encodePng(resizedImage);
-        widget.onImageSelected(Uint8List.fromList(
-            resizedBytes)); // Chama o callback passando a imagem redimensionada
         // Decodificar a imagem
         final img.Image? originalImage = img.decodeImage(imageBytes);
 
@@ -59,25 +55,58 @@ class _WidgetOpcoesImagemState extends State<WidgetOpcoesImagem> {
     }
   }
 
-
-
-@@ -66,9 +84,6 @@ class _WidgetOpcoesImagemState extends State<WidgetOpcoesImagem> {
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Usar câmera'),
+                onTap: () {
+                  _pickImage(ImageSource.camera);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo),
+                title: const Text('Escolher foto da galeria'),
+                onTap: () {
+                  _pickImage(ImageSource.gallery);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
                 leading: const Icon(Icons.close),
                 title: const Text('Remover Foto'),
                 onTap: () {
-                  setState(() {
-                    _imageData = null; // Remove a imagem
-                  });
                   widget
                       .onImageSelected(null); // Chama o callback passando null
                   Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
-@@ -92,7 +107,6 @@ class _WidgetOpcoesImagemState extends State<WidgetOpcoesImagem> {
+  @override
+  void initState() {
+    super.initState();
+    // Mostra a BottomSheet imediatamente após o widget ser construído
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _showBottomSheet(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox
-        .shrink(); // Retorna um SizedBox vazio, ocultando qualquer UI
     return const SizedBox.shrink(); // Retorna um SizedBox vazio
   }
 }
