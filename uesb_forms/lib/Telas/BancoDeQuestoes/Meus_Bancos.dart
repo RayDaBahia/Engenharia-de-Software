bf465545a5d08a64143326a6abf841f81738d6ad
Matheus Nepomuceno Souza <mnepomuceno2004@gmail.com>
2025-06-03 16:05:30 -0300

    design botão do banco

--- uesb_forms/lib/Telas/BancoDeQuestoes/Meus_Bancos.dart
+++ uesb_forms/lib/Telas/BancoDeQuestoes/Meus_Bancos.dart
@@ -22,25 +22,24 @@ class _MeusBancosState extends State<MeusBancos> {
     // Do not access context-dependent services here
   }
 
- 
   bool isFormulario = false; // Valor padrão
-bool _bancosCarregados = false; // Controle de carregamento
+  bool _bancosCarregados = false; // Controle de carregamento
 
-@override
-void didChangeDependencies() {
-  super.didChangeDependencies();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
 
-  final argumentos =
-      ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final argumentos =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
 
-  isFormulario = argumentos?['isFormulario'] ?? false;
+    isFormulario = argumentos?['isFormulario'] ?? false;
 
-  if (!_bancosCarregados) { // Impede chamadas múltiplas
-    Provider.of<BancoList>(context, listen: false).getBanco();
-    _bancosCarregados = true;
+    if (!_bancosCarregados) {
+      // Impede chamadas múltiplas
+      Provider.of<BancoList>(context, listen: false).getBanco();
+      _bancosCarregados = true;
+    }
   }
-}
-
 
   @override
   Widget build(BuildContext context) {
@@ -52,74 +51,81 @@ void didChangeDependencies() {
       drawer: isFormulario ? null : MenuLateral(),
       appBar: AppBar(
         title: Text(
-          isFormulario ? 'Meus Bancos' : 'Selecione o banco',        
+          isFormulario ? 'Meus Bancos' : 'Selecione o banco',
           style: TextStyle(color: Colors.white),
         ),
-        
+
         iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: const Color.fromARGB(255, 45, 12, 68),
         leading: isFormulario
             ? IconButton(
                 icon: const Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () {              
-               Navigator.of(context).pop(
-                MaterialPageRoute(
-                  builder: (context) => EdicaoQuestionario(), // Substitua por sua tela real
-                ),);
-      
+                onPressed: () {
+                  Navigator.of(context).pop(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          EdicaoQuestionario(), // Substitua por sua tela real
+                    ),
+                  );
                 },
               )
             : null, // Se não for formulário, mantém o padrão
       ),
-      body: Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            if (bancoList.bancosLista.isNotEmpty) ...[
-              WidgetPesquisa(
-                listaDeBancos: bancoList.bancosLista,
-                isFormulario: isFormulario,
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: bancoList.bancosLista.length,
-                  itemBuilder: (context, index) {
-                    final bancoQuestao = bancoList.bancosLista[index];
-                    return WidgetbancoQuestao(
-                        banco: bancoQuestao, isFormulario: isFormulario);
-                  },
-                ),
-              ),
-            ] else ...[
-              Center(
-                child: Text('Você não possui bancos'),
-              ),
-            ],
-            // Botão para adicionar novo banco
-            if(!isFormulario)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(Rotas.CRUD_BANCO);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 27, 7, 80),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
+                if (bancoList.bancosLista.isNotEmpty) ...[
+                  WidgetPesquisa(
+                    listaDeBancos: bancoList.bancosLista,
+                    isFormulario: isFormulario,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: bancoList.bancosLista.length,
+                      itemBuilder: (context, index) {
+                        final bancoQuestao = bancoList.bancosLista[index];
+                        return WidgetbancoQuestao(
+                            banco: bancoQuestao, isFormulario: isFormulario);
+                      },
                     ),
                   ),
-                  child: const Icon(
-                    Icons.add,
-                    size: 30,
-                    color: Colors.white,
+                ] else ...[
+                  Center(
+                    child: Text('Você não possui bancos'),
                   ),
-                ),
+                ],
               ],
-            )
-          ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: _botaoAdicionarBanco(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _botaoAdicionarBanco(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: ElevatedButton(
+        onPressed: () =>
+            Navigator.of(context).pushNamed(Rotas.CRUD_BANCO),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 45, 12, 68),
+          shape: const CircleBorder(),
+          padding: const EdgeInsets.all(20),
+        ),
+        child: const Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
         ),
       ),
     );
