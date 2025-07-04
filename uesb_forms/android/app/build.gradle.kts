78ec3af2374c621bd301e7bda195a858a351f803
Pedro Henrique Santos Souza Duarte <142743646+peudev10@users.noreply.github.com>
2025-07-03 21:53:52 -0300

    APK agora presta

--- uesb_forms/android/app/build.gradle.kts
+++ uesb_forms/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.form_uesb"
+        applicationId = "com.example.uesb_forms"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
